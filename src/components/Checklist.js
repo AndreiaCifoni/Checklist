db98257a342b1528757348d4d0158175b5642e1d
Andreia Cifoni <andreiacifoni@gmail.com>
2022-05-17 22:01:21 -0400

    checkbox functionality added

--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Checklist = () => {
-  return <div>Checklist</div>;
+  const [check, setCheck] = useState(false);
+
+  const onChangeHandler = (event) => {
+    setCheck(event.target.checked);
+  };
+
+  return (
+    <form>
+      <input type="checkbox" onChange={onChangeHandler} />
+      <label>ABC? {check ? "Yes" : "No"}</label>
+    </form>
+  );
 };
 
 export default Checklist;
